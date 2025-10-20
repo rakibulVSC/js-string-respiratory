7abd0da20ec3324206770d35ed60b0823d9f2c9d
rakibulVSC <mrakibulhasan755@gmail.com>
2025-10-20 19:16:01 +0600

    trim function

--- string.js
+++ string.js
@@ -1,9 +1,30 @@
-const country='Bangladesh';
-const divisioin='Dhaka';
-const district='Mymensingh';
-const thana='Phulpur';
+// const country='Bangladesh';
+// const divisioin='Dhaka';
+// const district='Mymensingh';
+// const thana='Phulpur';
 
-const numbers=[54,85,65,62,88]
-console.log(numbers.length)
+// const numbers=[54,85,65,62,88]
+// console.log(numbers.length)
 
-console.log(district.length)
+// console.log(district.length)
+
+const school='RAJ UK UTTARA MODEL SCHOOL';
+// console.log(school)
+// const subject='Chemistry';
+// const book='chemistry';
+// if(subject===book){
+//     console.log('I am reading book');
+// }
+// else{
+//     console.log('hudai hudai porsi')
+// }
+
+// console.log(school)
+// console.log(school.toLocaleLowerCase());
+// console.log(school.toUpperCase())
+
+const drink='water';
+const liquid=' water';
+if(drink === liquid.trim()){
+    console.log('It is same')
+}
