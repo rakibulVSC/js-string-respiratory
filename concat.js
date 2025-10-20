fa2a6f9c05bef97aea86ce26f3df2efcb7adabf3
rakibulVSC <mrakibulhasan755@gmail.com>
2025-10-20 19:58:53 +0600

    add object

--- concat.js
+++ concat.js
@@ -3,8 +3,19 @@ let reverse='';
 
 const sentences='I am learning web dev';
 
-for (const sentence of sentences){
-    // console.log(sentence)
-    reverse=sentence+reverse;
+// for (const sentence of sentences){
+//     // console.log(sentence)
+//     reverse=sentence+reverse;
+// }
+// console.log(reverse)
+
+let rev=0;
+
+for (let i = 0;i<=sentences.length;i++){
+
+
+    console.log(i)
+    console.log(sentences[i]);
+    const letter=sentences[i];
+    rev=letter+rev;
 }
-console.log(reverse)
---
+++ object.js
@@ -0,0 +1,20 @@
+const age=21;
+const school='rifle square';
+const isPassed=true;
+// let isDeveloper;
+const subjects=['bangla','physics','math'];
+const bottleo=['white',45];
+const bottleColor='white';
+const bottlePrice=45;
+const bottleDrinks='water';
+
+// object 
+
+const bottle={
+    brand:'apple',
+    price:45,
+    color:'white',
+    isClean:true
+
+}
+console.log(bottle)
