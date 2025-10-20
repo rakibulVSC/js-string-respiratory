f40a885e191d7463c1791bdaf981cf8bc1ef96c0
rakibulVSC <mrakibulhasan755@gmail.com>
2025-10-20 18:59:54 +0600

    add string length

---
+++ string.js
@@ -0,0 +1,9 @@
+const country='Bangladesh';
+const divisioin='Dhaka';
+const district='Mymensingh';
+const thana='Phulpur';
+
+const numbers=[54,85,65,62,88]
+console.log(numbers.length)
+
+console.log(district.length)
