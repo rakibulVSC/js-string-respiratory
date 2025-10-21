8b0fd89222d0f53c2129ec21789f660e9044a608
rakibulVSC <mrakibulhasan755@gmail.com>
2025-10-21 20:08:18 +0600

    add object properties

---
+++ value.js
@@ -0,0 +1,10 @@
+const computer={
+    brand:'lenovo',
+    price:45000,
+    processor:'ryzen',
+    hdd:'512gb'
+}
+const keys=Object.keys(computer);
+console.log(keys)
+const valus=Object.values(computer);
+console.log(valus)
