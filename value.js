18076f6db018147e566617b48cb0881110da2fca
rakibulVSC <mrakibulhasan755@gmail.com>
2025-10-21 20:24:38 +0600

    add prop

--- value.js
+++ value.js
@@ -1,10 +1,30 @@
-const computer={
-    brand:'lenovo',
-    price:45000,
-    processor:'ryzen',
-    hdd:'512gb'
+// const computer={
+//     brand:'lenovo',
+//     price:45000,
+//     processor:'ryzen',
+//     hdd:'512gb'
+// }
+// const keys=Object.keys(computer);
+// console.log(keys)
+// const valus=Object.values(computer);
+// console.log(valus)
+// delete computer.hdd;
+// console.log(computer)
+
+
+// loop
+
+const mobile={
+    brand:'samsung',
+    price:25000,
+    color:'black',
+    camera:'12mp,'
 }
-const keys=Object.keys(computer);
-console.log(keys)
-const valus=Object.values(computer);
-console.log(valus)
+
+// for (const prop in mobile){
+//     console.log(prop)
+//     console.log(mobile[prop])
+// }
+
+// const mobiles=new Object();
+// console.log(mobiles)
