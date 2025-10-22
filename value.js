1959bcdaf4cffc8101596fc98ccc95098724886b
rakibulVSC <mrakibulhasan755@gmail.com>
2025-10-22 09:24:13 +0600

    add loop an object

--- value.js
+++ value.js
@@ -28,3 +28,59 @@ const mobile={
 
 // const mobiles=new Object();
 // console.log(mobiles)
+
+// const colors = {
+//     red: "#ff0000",
+//     green: "#00ff00",
+//     blue: "#0000ff",
+//     "golden rod": '#daa520'
+// }
+// console.log(colors['golden rod'])
+
+// const car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020
+// };
+// car['passanger capacity']=5;
+// console.log(car)
+
+// const student = {
+//     name: "Hamim Sakep",
+//     id: 5421,
+//     physics: {
+//         subject: "HSC Physics",
+//         author: "Shahjahan Tapan",
+//         marks: 30
+//     }
+// };
+// console.log(student.physics.marks)
+
+// let student = {
+//     name: 'Ariana Grande',
+//     age: 21,
+//     city: 'Gaibandha',
+//     isStudent: true
+// };
+// const students=Object.keys(student)
+// console.log(students.length)
+
+let myObject = {
+
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+// for(const prop in myObject){
+//     console.log(prop);
+//     console.log(myObject[prop])
+// }
+const keys=Object.keys(myObject);
+
+
+for(const key of keys){
+    const valu=myObject[key];
+    const type=typeof valu;
+    console.log("key:", key ,'|', 'type:',type)
+}
